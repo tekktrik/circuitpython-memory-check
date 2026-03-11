fe5d610dbbf83276668c890900c637aa2025bdf4
Alec Delaney <tekktrik@gmail.com>
2026-03-10 20:59:26 -0400

    Initial prototype

---
+++ scripts/analyze.py
@@ -0,0 +1,51 @@
+import os
+import pathlib
+import tomllib
+
+import circuitpy_sim
+
+with open("pyproject.toml", mode="rb") as tomlfile:
+    pyproject = tomllib.load(tomlfile)
+
+setuptools = pyproject["tool"]["setuptools"]
+
+circuitpy_folder = os.path.join(os.getcwd(), "CIRCUITPY")
+
+if "py-modules" in setuptools:
+    filepaths = [setuptools["py-modules"][0] + ".py"]
+elif "packages" in setuptools:
+    filepaths = [name.replace(".", "/") for name in setuptools["packages"]]
+else:
+    raise KeyError("Neither modules nor packages are defined in pyproject.toml")
+
+paths = [pathlib.Path(filepath) for filepath in filepaths]
+
+import_paths: set[pathlib.Path] = set()
+for path in paths:
+    import_paths.add(path)
+    if path.is_dir():
+        for matching_file in path.glob("**/*.py"):
+            matching_name = matching_file.parent if matching_file.name in ("__init__.py", "__main__.py") else matching_file
+            import_paths.add(matching_name)
+
+import_names = set()
+for path in import_paths:
+    if not path.is_file() and not (path / "__init__.py").is_file():
+        continue
+    path_like = str(path).replace("/", ".")
+    if path_like.endswith(".py"):
+        path_like = path_like[:-3]
+    import_names.add(path_like)
+
+simulator = circuitpy_sim.Simualtor()
+
+for name in import_names:
+    code_file = pathlib.Path("CIRCUITPY/code.py")
+    code_text = (
+        f"print({name})"
+    )
+    with open(code_file, mode="w") as code_fp:
+        code_fp.write(code_text)
+
+    result = simulator.simulate()
+    print(result)
---
+++ scripts/copy.py
@@ -0,0 +1,19 @@
+import shutil
+import tomllib
+import os
+
+with open("pyproject.toml") as tomlfile:
+    pyproject = tomllib.load(tomlfile)
+
+setuptools = pyproject["tool"]["setuptools"]
+
+circuitpy_folder = os.path.join(os.getcwd(), "CIRCUITPY")
+
+if "py-modules" in setuptools:
+    library_file = setuptools["py-modules"][0] + ".py"
+    shutil.copy(library_file, circuitpy_folder)
+elif "packages" in setuptools:
+    library_folder = setuptools["packages"][0]
+    shutil.copytree(library_folder, circuitpy_folder, dirs_exist_ok=True)
+else:
+    raise KeyError("Neither modules nor packages are defined in pyproject.toml")
