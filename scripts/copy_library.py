a0cc08ddc6f5e28a36ee5096f09d358d6719e920
Alec Delaney <tekktrik@gmail.com>
2026-03-13 14:38:00 -0400

    Fix copying library

--- scripts/copy_library.py
+++ scripts/copy_library.py
@@ -10,13 +10,15 @@ setuptools = pyproject["tool"]["setuptools"]
 
 circuitpy_folder = os.path.join(os.getcwd(), "CIRCUITPY")
 
-pathlib.Path(circuitpy_folder).mkdir()
+os.mkdir(circuitpy_folder)
 
 if "py-modules" in setuptools:
     library_file = setuptools["py-modules"][0] + ".py"
     shutil.copy(library_file, circuitpy_folder)
 elif "packages" in setuptools:
     library_folder = setuptools["packages"][0]
-    shutil.copytree(library_folder, circuitpy_folder, dirs_exist_ok=True)
+    drive_folder = os.path.join(circuitpy_folder, library_folder)
+    os.mkdir(drive_folder)
+    shutil.copytree(library_folder, drive_folder, dirs_exist_ok=True)
 else:
     raise KeyError("Neither modules nor packages are defined in pyproject.toml")
