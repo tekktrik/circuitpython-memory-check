184d20a5c28bd7ff5ed14591339b8ce724b63aad
Alec Delaney <tekktrik@gmail.com>
2026-03-14 17:02:16 -0400

    Update to use simulator static method

--- scripts/check_memory.py
+++ scripts/check_memory.py
@@ -7,9 +7,7 @@ firmware_filepath = sys.argv[1]
 flash_filepath = sys.argv[2]
 import_name = sys.argv[3]
 
-simulator = circuitpy_sim.Simualtor(firmware_filepath, flash_filepath)
-
-result = simulator.simulate()
+result = circuitpy_sim.Simualtor.simulate(firmware_filepath, flash_filepath)
 result_json = json.dumps(result)
 
 with open(import_name + ".json", mode="w") as jsonfile:
